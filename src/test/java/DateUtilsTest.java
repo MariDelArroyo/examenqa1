874426e313c08ff0c064e2e1815b05c7b87e140d
Mariana <delarroyorengelmariana@gmail.com>
2024-10-07 18:24:02 -0400

    Commit 3 examen

---
+++ src/test/java/DateUtilsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DateUtilsTest {
+    @Test
+    public void testValidarDateofWeekTh(){
+        DateUtils dateUtils = new DateUtils();
+        String result = dateUtils.getDayOfWeek("2024-02-29");
+        Assertions.assertEquals("THURSDAY", result);
+    }
+    @Test
+    public void testValidarDateofWeekMo(){
+        DateUtils dateUtils = new DateUtils();
+        String result = dateUtils.getDayOfWeek("2024-01-01");
+        Assertions.assertEquals("MONDAY", result);
+    }
+}
